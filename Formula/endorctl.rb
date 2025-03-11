5e44ded1714f51ddc81f0fb1bf54aefb4e42236a
Derek Francour <dfrancour@endor.ai>
2025-03-11 15:16:38 -0700

    Testing commit - Support brew upgrade endorctl

---
+++ Formula/endorctl.rb
@@ -0,0 +1,66 @@
+require 'net/http'
+
+class EndorDownloadStrategy < CurlDownloadStrategy
+  def url
+    versioned_url = $download_url
+    versioned_url
+  end
+end
+
+class Endorctl < Formula
+  homepage "https://github.com/endorlabs/homebrew-tap"
+  desc "Endor Labs Homebrew Tap"
+
+  ENDORCTL_VERSION_URL = "https://api.endorlabs.com/meta/version"
+
+  livecheck do
+    url ENDORCTL_VERSION_URL
+    strategy :json do |json|
+      json["ClientVersion"]
+    end
+  end
+
+  def self.fetch_version_json
+    uri = URI(ENDORCTL_VERSION_URL)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    req = Net::HTTP::Get.new(uri)
+    res = http.request(req)
+    if res.is_a?(Net::HTTPSuccess)
+      version_json = JSON.parse(res.body)
+      version_json
+    else
+      odie "Failed to get version metadata"
+    end
+  end
+
+  version_json = fetch_version_json
+  version version_json["ClientVersion"]
+
+  # Assume MacOS, ARM
+  use_arch = "arm64"
+  $download_sha = version_json["ClientChecksums"]["ARCH_TYPE_MACOS_ARM64"]
+
+  on_macos do
+    on_intel do
+      use_arch = "amd64"
+      $download_sha = version_json["ClientChecksums"]["ARCH_TYPE_MACOS_AMD64"]
+    end
+  end
+
+  $endorctl_file = "endorctl_#{version}_macos_#{use_arch}"
+  $download_url = "https://api.endorlabs.com/download/endorlabs/#{version}/binaries/#{$endorctl_file}"
+
+  # Download and install
+  url "about:blank", :using => EndorDownloadStrategy  # actual URL determined by download strategy
+  
+  def install
+     bin.install "#{$endorctl_file}" => "endorctl"
+  end
+
+  test do
+    system "#{bin}/endorctl", "--version"
+  end
+
+end
